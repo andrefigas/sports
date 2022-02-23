a50a63acb88c4a50f5d8f62e2de5f01d082b7488
andrefigas <andre.ce89@gmail.com>
2022-02-23 10:04:09 +0000

    - database (mock) layer - favorite feature

--- app/src/main/java/andrefigas/com/github/sports/view/MainActivity.kt
+++ app/src/main/java/andrefigas/com/github/sports/view/MainActivity.kt
@@ -10,7 +10,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
-class MainActivity() : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var presenter: EventListPresenterContract
